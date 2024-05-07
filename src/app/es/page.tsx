d7246375fe5f35c77533421060c24c82a9ec364d
LihuelZerega <zeregalihuel@gmail.com>
2024-05-07 09:56:30 -0300

    Footer

--- src/app/es/page.tsx
+++ src/app/es/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./subcomps/NavBar";
 import HeroSection from "./subcomps/HeroSection";
+import Stats from "./subcomps/Stats";
 import Footer from "./subcomps/Footer";
 
 function page() {
@@ -8,6 +9,7 @@ function page() {
     <div className="bg-white">
       <NavBar />
       <HeroSection />
+      <Stats />
       <Footer />
     </div>
   );
